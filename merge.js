46dabb3e8d6fd5ac0ffab6a93405421c27aafc38
Przemysław Sporysz <p.sporysz@gmail.com>
2013-07-06 21:21:05 +0200

    merges pages and appends to the body

--- merge.js
+++ merge.js
@@ -3,5 +3,10 @@ $(function(){
     var paginatorEl = $('a:contains(1) + a:contains(2)').parent();
 
     // get content from all pages
-    paginatorEl.find('a').hide();
+    paginatorEl.find('a').each(function(){
+        var url = $(this).attr('href');
+        $.ajax(url).done(function(data){
+            $('body').append(data);
+        });
+    });
 });
